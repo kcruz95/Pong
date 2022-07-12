31d32cbb7d95eeb39e868e706230bfa5bb6d4bc6
kcruz95 <kyle.cruz@hotmail.ca>
2022-07-11 19:26:44 -0700

    ball now moves in randomised direction

--- ball.js
+++ ball.js
@@ -1,6 +1,10 @@
+const INITIAL_VELOCITY = 0.025
+// ball velocity
+
 export default class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem
+    this.reset()
   }
 
   get x() {
@@ -11,7 +15,37 @@ export default class Ball {
     this.ballElem.style.setProperty("--x", value)
   }
 
-  update(delta) {
-    this.x = 5
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
   }
-}
+
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value)
+  }
+
+  reset() {
+    this.x = 50
+    this.y = 50
+    this.direction = {
+      x: 0
+    }
+    while(Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9) {
+      const heading = randomNumberBetween(0, 2 * Math.PI)
+      this.direction= {
+        x: Math.cos(heading),
+        y: Math.sin(heading)
+      }
+      this.velocity = INITIAL_VELOCITY
+    };
+    console.log(this.direction)
+  };
+
+  update(delta) {
+    this.x += this.direction.x * this.velocity * delta
+    this.y += this.direction.y * this.velocity * delta
+  };
+};
+
+function randomNumberBetween (min, max) {
+  return Math.random() * (max - min) +  min
+};
